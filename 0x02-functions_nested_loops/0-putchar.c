b5b2dbcd9649800e70c3360609b65ea5f23ba4d1
lamiae777 <lamiae.serf@gmail.com>
2023-07-18 10:25:10 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,19 @@
 #include "main.h"
-
 /**
- * main - Prints Holberton as a message.
- *
- * Return: Always 0 (Success)
+ * main - Print _putchar
+ *is written by mbah
+ * Return: 0
  */
 int main(void)
 {
-   _putchar('_');
-   _putchar('p');
-   _putchar('u');
-   _putchar('t');
-   _putchar('c');
-   _putchar('h');
-   _putchar('a');
-   _putchar('r');
-   _putchar('\n');
-   return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
