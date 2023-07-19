562569a5ad53b6406d9dd9368bc0cde9903f0182
lamiae777 <lamiae.serf@gmail.com>
2023-07-19 08:41:55 -0700

    yarbi yarbi

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,50 +12,26 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-    if (year % 4 == 0)
+    if (year % 4 == 0 || (year % 400 == 0 && year % 100 == 0))
     {
-        if (year % 100 == 0)
+        if (month >= 2 && day >= 60)
         {
-            if (year % 400 == 0)
-            {
-                // Leap year, including 2000
-                if (month >= 2 && day >= 60)
-                {
-                    day++;
-                }
-                printf("Day of the year: %d\n", day);
-                printf("Remaining days: %d\n", 366 - day);
-            }
-            else
-            {
-                // Not a leap year, like 1900
-                if (month == 2 && day == 60)
-                {
-                    printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-                }
-                else
-                {
-                    printf("Day of the year: %d\n", day);
-                    printf("Remaining days: %d\n", 365 - day);
-                }
-            }
+            day++;
         }
-        else
-        {
-            // Leap year, excluding those divisible by 100 but not by 400
-            if (month >= 2 && day >= 60)
-            {
-                day++;
-            }
-            printf("Day of the year: %d\n", day);
-            printf("Remaining days: %d\n", 366 - day);
-        }
+
+        printf("Day of the year: %d\n", day);
+        printf("Remaining days: %d\n", 366 - day);
     }
     else
     {
-        // Not a leap year
-        printf("Day of the year: %d\n", day);
-        printf("Remaining days: %d\n", 365 - day);
+        if (month == 2 && day == 60)
+        {
+            printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+        }
+        else
+        {
+            printf("Day of the year: %d\n", day);
+            printf("Remaining days: %d\n", 365 - day);
+        }
     }
-}
-
+}}
